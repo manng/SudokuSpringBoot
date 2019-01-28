b452eedfd50d5d234cc4ff53ef25037209943048
manngs <gary.mann1@optusnet.com.au>
2019-01-28 19:15:10 +1100

    Renamed files from SpringBotApplication to SpringBootApplication

--- src/main/java/sudoku/SudokuSpringBootApplication.java
+++ src/main/java/sudoku/SudokuSpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SudokuSpringBotApplication {
+public class SudokuSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SudokuSpringBotApplication.class, args);
+		SpringApplication.run(SudokuSpringBootApplication.class, args);
 	}
 
 }
